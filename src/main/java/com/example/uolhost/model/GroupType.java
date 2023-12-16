0c214808f106e40c96cabe9825e31afa1c376471
Lucas <lucas.desenvolvedor@hotmail.com>
2023-12-16 18:33:47 -0300

    create model

---
+++ src/main/java/com/example/uolhost/model/GroupType.java
@@ -0,0 +1,8 @@
+package com.example.uolhost.model;
+
+public enum GroupType {
+
+    JUSTICE_LEAGUE,
+    AVANGERS
+
+}
---
+++ src/main/java/com/example/uolhost/model/Player.java
@@ -0,0 +1,31 @@
+package com.example.uolhost.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "players")
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class Player {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    private String name;
+    @NotBlank
+    private String email;
+    private String phoneNumber;
+    private String codiname;
+    private GroupType groupType;
+
+}
